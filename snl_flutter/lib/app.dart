657a0704266cfef8c1b790658d74bf3890c39818
FlorianFlouquet <64949896+FlorianFlouquet@users.noreply.github.com>
2023-04-08 19:46:29 +0200

    feat(homepage): made homepage interface (#1)

---
+++ snl_flutter/lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:snl_flutter/presentation/ui/home_page.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SnapNLearnApp extends StatelessWidget {
+  const SnapNLearnApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
